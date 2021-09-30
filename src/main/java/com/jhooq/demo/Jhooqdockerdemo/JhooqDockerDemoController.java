b246d3dafa8bbbcd21f9ef66cac9688410b3d727
yenne375 <56918424+yenne375@users.noreply.github.com>
2021-09-30 17:23:23 +0530

    Update JhooqDockerDemoController.java

--- src/main/java/com/jhooq/demo/Jhooqdockerdemo/JhooqDockerDemoController.java
+++ src/main/java/com/jhooq/demo/Jhooqdockerdemo/JhooqDockerDemoController.java
@@ -8,6 +8,6 @@ public class JhooqDockerDemoController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Docker Demo - Hello Jhooq";
+        return "Argocd demo...! cheers Jagadeesh";
     }
 }
